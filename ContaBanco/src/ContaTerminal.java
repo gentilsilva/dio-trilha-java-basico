69e264e11d6b5a13ff3b0e4ebcd10aea3a81942b
Gentil Silva <silvagentil.sg@gmail.com>
2024-04-02 01:05:30 -0300

    challenge finished

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scan = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;        
+
+        System.out.print("Por favor, digite o número da conta: ");
+        numero = Integer.parseInt(scan.nextLine());
+
+        System.out.print("Por favor, digite a agência: ");
+        agencia = scan.nextLine();
+
+        System.out.print("Por favor, digite o seu nome: ");
+        nomeCliente = scan.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        saldo = scan.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+        "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque." );
     }
 }
